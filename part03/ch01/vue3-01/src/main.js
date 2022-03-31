ff22d338b15b2e5073847ce97c90ac8b9c0c0e0f
leejeongpil <ijpsm9@gmail.com>
2022-03-31 10:52:23 +0900

    asd

--- part03/ch01/vue3-01/src/main.js
+++ part03/ch01/vue3-01/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// add axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// add bootstrap, bootstrap vue3
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+// createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios)
+app.use(BootstrapVue3)
+app.mount('#app')
