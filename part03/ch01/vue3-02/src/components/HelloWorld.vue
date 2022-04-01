87fc9b571e551159a719508607d33ca79528d830
leejeongpil <ijpsm9@gmail.com>
2022-04-01 15:09:08 +0900

    asd

--- part03/ch01/vue3-02/src/components/HelloWorld.vue
+++ part03/ch01/vue3-02/src/components/HelloWorld.vue
@@ -1,19 +1,43 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <button @click="clickButt">nameList is</button>
+  <ul>
+    <li v-for="(name, index) in nameList" :key="index">
+      {{ index }}:{{ name }}
+    </li>
+  </ul>
+  <ul>
+    <li v-for="(name, index) in nameList2" :key="index">
+      {{ index }}:{{ name }}
+    </li>
+  </ul>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
-}
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+const count = ref(0);
+const nameList = ref([]);
+const nameList2 = ref([]);
+
+const clickButt = async () => {
+  await axios
+    .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+    .then((res, err) => {
+      console.log(res);
+      let dataList = res.data;
+      nameList2.value = dataList.map((comment) => comment.name);
+      dataList.forEach((element) => {
+        // console.log(element.name);
+        // nameList.value.filter((element) => element.name);
+        nameList.value.push(element.name);
+      });
+      // nameList.value = res.data.name;
+    })
+    .catch((err) => console.log(err));
+};
 </script>
